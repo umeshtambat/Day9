3569351e786bc5e73bb7d65a93eab64e5c393718
Umesh <Umeshtambat55@gmail.com>
2023-06-09 15:53:46 +0530

    internal module read the file

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
+
+
+//how to read a file using Nodejs?
+import { isUtf8 } from "node:buffer";
 import { sum } from "./calc.js";
 
+//how to read a file using Nodejs?
+//this is an internal module
+import {readFileSync} from "node:fs";
 
-let output  =  sum(10,30);
-console.log(output);
+
+let filePath = "/Users/Shweta/Desktop/New folder/Day9/package.json"; 
+
+let fileData = readFileSync(filePath ,{encoding: 'Utf-8'});
+console.log(fileData);
+
