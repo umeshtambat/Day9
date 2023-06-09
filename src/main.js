235aab397f3350109d53923b6ceaae25b6334afd
Umesh <Umeshtambat55@gmail.com>
2023-06-09 21:45:46 +0530

    asyc version of write file

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 
 
-import { readFile } from 'node:fs/promises';
+import { writeFile } from 'node:fs/promises';
+
 
 async function main () {
-    let filePath  = "C:/Users/Shweta/Desktop/New folder/Day9/package.json";
-let fileData =  await readFile(filePath , {encoding: "utf-8"});
-console.log(fileData);
-
+    
+let filePath = "C:/Users/Shweta/Desktop/New folder/Day9/output1.txt";
+let fileData = `The optional options argument can be a string specifying an encoding, 
+or an object with an encoding property specifying the character encoding to use for the link path returned. 
+If the encoding is set to 'buffer', the link path returned will be passed as a <Buffer> object.`;
+ await writeFile(filePath, fileData);
+ console.log("success");
 }
+
+
 
 main();
