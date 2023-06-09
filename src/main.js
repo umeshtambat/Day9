e7eba08440c8090f011155243a1f2401c12da311
Umesh <Umeshtambat55@gmail.com>
2023-06-09 21:07:40 +0530

    async and await

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@
 
 // 
 async function sum(n1, n2){
-    return n1 + n2;
+    return (n1 + n2);
 }
 
-let output = sum(10,20);
+ async function main() {
+let output =  await sum(10,20);
 console.log(output);
+ }
+
+ main();
